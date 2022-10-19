644949673d29f71e4620293eb585d16363ce7b23
ajtayde001 <ajtayde001@gmail.com>
2022-10-19 15:44:48 +0530

    problem is solved

--- palindrome.js
+++ palindrome.js
@@ -2,15 +2,11 @@
  //Check whether a string is palindrome or not.
 
 let str = "naman"
-let row1 = "";
+let bag = "";
 for(let i=str.length-1;i>=0;i--){
-  row1=row1+str[i];
+  bag=bag+str[i];
 }
-let row2 = ""
-for(let i=0;i<=str.length-1;i++){
-  row2=row2+str[i];
-}
-if(row1==row2){
+if(str==bag){
   console.log("Yes")
 }else{
   console.log("No")
